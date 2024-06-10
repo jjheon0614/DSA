55f2be947646be67fca44f59e9db338a8ad1f4c6
jaeheon <jjheon0614@gmail.com>
2024-06-11 00:53:19 +0900

    1158 code update

--- Stack&Queue/1158.cpp
+++ Stack&Queue/1158.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+
+using namespace std;
 
 int main() {
 
+    int N, K;
+    cin >> N >> K;
+    queue<int> q;
+
+    for (int i = 1; i <= N; i++) {
+        q.push(i);
+    }
+
+    vector<int> v;
+    int count = 1;
+    while (!q.empty()) {
+        int num = q.front();
+        q.pop();
+        if (count == K) {
+            v.push_back(num);
+            count = 1;
+            continue;
+        } else {
+            count++;
+            q.push(num);
+        }
+    }
+
+    cout << '<';
+    for (int i = 0; i < N; i++) {
+        if (i == 0) {
+            cout << v[i];
+        } else {
+            cout << ", " << v[i];
+        }
+    }
+    cout << ">\n";
 
 
     return 0;
