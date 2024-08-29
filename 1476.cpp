ae2b0cd4a27de98326451ccbcb6d2569aa490128
jaeheon <jjheon0614@gmail.com>
2024-08-29 23:24:15 +0700

    update

---
+++ 1476.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int E, S, M;
+
+int main() {
+    cin >> E >> S >> M;
+
+    if (E == S && E == M) {
+        cout << E << "\n";
+
+        return 0;
+    }
+
+    int count = 1;
+
+    while (true) {
+
+    }
+
+    return 0;
+}
