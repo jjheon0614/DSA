9c3030e893a4f0f6a31674a6b59a069c712a309c
jaeheon <jjheon0614@gmail.com>
2025-01-07 17:21:24 +0700

    programmers update

---
+++ BFS/부대복귀.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> edge[100001];
+
+vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
+    vector<int> answer(sources.size());
+    vector<int> costFromDestination(n + 1, -1);
+    
+    for (int i = 0; i < roads.size(); i++) {
+        int start = roads[i][0];
+        int dest = roads[i][1];
+        
+        edge[start].push_back(dest);
+        edge[dest].push_back(start);
+    }
+    
+    queue<pair<int, int>> q;
+    q.push({destination, 0});
+    costFromDestination[destination] = 0;
+    
+    while (!q.empty()) {
+        auto curPos = q.front().first;
+        auto curCount = q.front().second;
+        q.pop();
+        
+        for (int i = 0; i < edge[curPos].size(); i++) {
+            int nextPos = edge[curPos][i];
+            
+            if (costFromDestination[nextPos] == -1 || costFromDestination[nextPos] > curCount + 1) {
+                q.push({nextPos, curCount + 1});
+                costFromDestination[nextPos] = curCount + 1;
+            } 
+        }
+    }
+    
+    for (int i = 0; i < sources.size(); i++) {
+        answer[i] = costFromDestination[sources[i]];
+    }
+    
+    return answer;
+}
