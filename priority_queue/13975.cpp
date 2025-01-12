f7f03ef71f3d859aaf4e90e7e2728053c6f48a78
jaeheon <jjheon0614@gmail.com>
2025-01-13 00:47:32 +0700

    edit 13975.cpp

--- priority_queue/13975.cpp
+++ priority_queue/13975.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
 
 int main() {
 
-    
+    int t;
+    cin >> t;
 
+    int k;
+
+    for (int i = 0; i < t; i++) {
+        cin >> k;
+        priority_queue<long long, vector<long long>, greater<long long>> minHeap;
+        for (int j = 0; j < k; j++) {
+            long long ch;
+            cin >> ch;
+            minHeap.push(ch);
+        }
+
+        long long result = 0;
+
+        while (minHeap.size() != 1) {
+            long long min = minHeap.top();
+            minHeap.pop();
+            long long secondMin = minHeap.top();
+            minHeap.pop();
+
+            long long sum = min + secondMin;
+            result += sum;
+
+            minHeap.push(sum);
+        }
+
+        cout << result << "\n";
+    }
 
     return 0;
 }
